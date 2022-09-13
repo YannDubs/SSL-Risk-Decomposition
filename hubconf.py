b885d50d49ebb915dbb3f5325c25f2061c215ab1
Yann Dubois <yanndubois96@gmail.com>
2022-09-13 01:30:04 -0700

    addd disll meta data

--- hubconf.py
+++ hubconf.py
@@ -3,10 +3,12 @@ dependencies = [ "torch", "torchvision", "yaml", "pandas"]
 
 import yaml
 from pathlib import Path
-from pretrained import available_models, load_representor
+
 
 BASE_DIR = Path(__file__).absolute().parents[0]
 
 def metadata():
     with open(BASE_DIR/'metadata.yaml') as f:
         return yaml.safe_load(f)
+
+from pretrained import available_models, load_representor
