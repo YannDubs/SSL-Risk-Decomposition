5379eb822a1f5659c357d506eb2a144b874e2b53
Yann Dubois <yanndubois96@gmail.com>
2022-09-18 21:43:40 -0700

    add mugs models

---
+++ scripts/mugs.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+experiment="vit"
+notes="**Goal**: evaluate all the ibot models."
+
+# parses special mode for running the script
+source `dirname $0`/utils.sh
+
+
+# define all the arguments modified or added to `conf`. If they are added use `+`
+kwargs="
+experiment=$experiment
+timeout=$time
+"
+
+kwargs_multi="
+representor=mugs_vits16_ep100,mugs_vits16_ep300,mugs_vits16_ep800,mugs_vitb16_ep400,mugs_vitl16_ep250,mugs_vits16_ep800_extractS,mugs_vitb16_ep400_extractB
+seed=123
+predictor=torch_linear
+"
+
+kwargs_multi="
+representor=mugs_vits16_ep800_extractS,mugs_vitb16_ep400_extractB
+seed=123
+predictor=torch_linear
+"
+
+if [ "$is_plot_only" = false ] ; then
+  for kwargs_dep in ""
+  do
+
+    python "$main" +hydra.job.env_set.WANDB_NOTES="\"${notes}\"" $kwargs $kwargs_multi $kwargs_dep $add_kwargs -m >> logs/"$experiment".log 2>&1 &
+
+    sleep 10
+
+  done
+fi
+
